5012d333a0f49665d7e8b12942a7c112badbeb9e
AlexKr <kaf-2012@mail.ru>
2022-12-06 23:26:50 +0300

    задача №8 выполнена

---
+++ HomeWork/sem1/task8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write(" Введите число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+bool first = false;
+
+for (int i = 1; i <= number1; i++)
+{
+    if (i % 2 == 0)
+    {
+        if (first == true)
+        { Console.Write(" , " + i); }
+        else
+        {
+            first = true;
+            Console.Write(i);
+        }
+    }
+}
