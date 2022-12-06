ac75915384ad5f2945ddfe07d68bb336840f1a3d
AlexKr <kaf-2012@mail.ru>
2022-12-06 23:24:27 +0300

    задача №6 выполнена

---
+++ HomeWork/sem1/task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+
+string number1 = Console.ReadLine()!;
+
+int parsed1 = int.Parse(number1);
+
+if (parsed1 % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
