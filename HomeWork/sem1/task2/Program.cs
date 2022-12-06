24ac6f1a4b07dc03513215a3596f8045f56e967e
AlexKr <kaf-2012@mail.ru>
2022-12-06 23:10:50 +0300

    задача №2 выполнена

---
+++ HomeWork/sem1/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите 2 числа ");
+
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+
+if(number1 == number2)
+{
+    Console.Write(" Числа равны ");
+} else if (number1 > number2)
+{
+    Console.WriteLine(" Первое число больше: " + number1 );
+} else
+{
+    Console.WriteLine(" Второе число больше: " + number2);
+}
