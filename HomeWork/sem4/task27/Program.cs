c35f4f383297bc585fb4640a62baca96340404af
AlexKr <kaf-2012@mail.ru>
2022-12-11 22:09:03 +0300

    задача № 27 выполнена

---
+++ HomeWork/sem4/task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sum = sumDigit(number);
+Console.WriteLine(sum);
+
+int sumDigit(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        int num = n % 10;
+        n = n / 10;
+        sum = sum + num;
+    }
+    return sum;
+}
