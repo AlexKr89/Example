faaf6968ed3fe647efa468e449eca20a27993cd0
AlexKr <kaf-2012@mail.ru>
2022-12-12 22:45:44 +0300

    Example 10, 11 изменены

--- Exampless/Lession2/Example010MethodArray/Program.cs
+++ Exampless/Lession2/Example010MethodArray/Program.cs
@@ -1,67 +1,17 @@
-﻿// int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+﻿int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
 
-// int n = array.Length;
-// int find = 18;
+int n = array.Length;
+int find = 17;
 
-// int index = 0;
+int index = 0;
 
-// while (index < n)
-// {
-//     if (array[index] == find)
-//     {
-//         Console.WriteLine(index);
-//         break;
-//     }
-//     // index = index +1;
-//     index++;
-// }
-void FillArray(int[] collection)
+while (index < n)
 {
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
+    if (array[index] == find)
     {
-        collection[index] = new Random().Next(1, 10);
-        index++;
+        Console.WriteLine(index);
+        break;
     }
+    // index = index +1;
+    index++;
 }
-void PrintArray(int[] col)
-{
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.WriteLine(col[position]);
-        position++;
-    }
-}
-
-int Index0f(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
-    while (index < count)
-    {
-        if (collection[index] == find)
-        {
-            position = index;
-            break;
-        }
-        index++;
-    }
-    return position;
-}
-
-
-
-int[] array = new int[10];
-
-FillArray(array);
-array[4] = 4;
-array[6] = 4;
-PrintArray(array);
-Console.WriteLine();
-
-int pos = Index0f(array, 444);
-Console.WriteLine(pos);
