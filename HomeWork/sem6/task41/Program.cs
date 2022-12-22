d373b09dbd54f69fbb62469d852b5ed79bd6030f
AlexKr <kaf-2012@mail.ru>
2022-12-22 23:51:52 +0300

    задача № 41 выполнена

--- HomeWork/sem6/task41/Program.cs
+++ HomeWork/sem6/task41/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество цифр: ");
+int number = int.Parse(Console.ReadLine()!);
+int[] arr = new int[number];
+
+Console.WriteLine($"Количество цифр больше нуля -> {CounterPosNum(UserFillArray(arr))}");
+
+int[] UserFillArray(int[] array)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число");
+        arr[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int CounterPosNum(int[] array)
+{
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
