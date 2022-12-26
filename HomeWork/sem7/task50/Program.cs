83c572b76332fe7f773a604c037bd1efa1cdeafb
AlexKr <kaf-2012@mail.ru>
2022-12-27 01:09:35 +0300

    задача № 50 выполнена

---
+++ HomeWork/sem7/task50/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1
+// 7 -> такого числа в массиве нет
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] myMatrix = Create2DArray(m, n, 0, 10);
+Print2DArray(myMatrix);
+
+Console.WriteLine("Введите номер строки");
+int line = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите номер столбца");
+int col = int.Parse(Console.ReadLine()!);
+
+if (line > m || col > n) Console.WriteLine("такого числа в массиве нет");
+else Console.WriteLine(myMatrix[line - 1, col - 1]);
+
+int[,] Create2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
