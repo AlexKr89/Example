6b9a909e98b70a39a9b11945fc64c6e7d184af1c
AlexKr <kaf-2012@mail.ru>
2022-12-27 01:11:09 +0300

    задача № 52 выполнена

---
+++ HomeWork/sem7/task52/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int[,] myMatrix = Create2DArray(m, n, 1, 10);
+Print2DArray(myMatrix);
+Console.WriteLine();
+
+for (int j = 0; j < myMatrix.GetLength(1); j++)
+{
+    double sumCol = 0;
+    int i = 0;
+    while (i < myMatrix.GetLength(0))
+    {
+        sumCol += myMatrix[i, j];
+        i++;
+    }
+    double avgVal = sumCol / i;
+    Console.Write(Math.Round(avgVal, 1) + "; ");
+}
+
+int[,] Create2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
